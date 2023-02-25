bb126b8ae935d5c9a9542e948617c16475207397
pabonhomme1 <paul.bonhomme@etu.uca.fr>
2023-02-25 23:04:25 +0100

    [DEV] CONNEXION working

--- igardenconnectweb/utils/cookie.js
+++ igardenconnectweb/utils/cookie.js
@@ -1,45 +1,46 @@
 import { UserVM } from "@/model/UserVM";
-import { Cookie } from "../model/Cookie"
+import { Cookie } from "../model/Cookie";
 
 export function setSessionCookie(user) {
   fetch(`http://localhost:5241/api/User/cookie/${user.idUser}`)
-    .then(response => {
+    .then((response) => {
       if (!response.ok) {
-        throw new Error('Failed to retrieve session cookie');
+        throw new Error("Failed to retrieve session cookie");
       }
       return response.json();
     })
-    .then(cookie => {
+    .then((cookie) => {
       // Ajouter le cookie à document.cookie
+      console.log(cookie);
       document.cookie = `sessionCookie=${cookie.value};expires=${cookie.expires};path=/`;
     })
-    .catch(error => {
+    .catch((error) => {
       console.error(error);
     });
 }
 
 export async function getUserByToken() {
-    return new Promise(async (resolve, reject) => {
-        let user = new UserVM();
-        if (document.cookie) {
-            console.log(document.cookie);
-            // get cookie
-            const sessionCookie = document.cookie.split('; ').find(row => row.startsWith('sessionCookie='));
-            const cookieValue = sessionCookie.split('=')[1];
-        
-            console.log(cookieValue);
-            if (cookieValue) {
-                
-                // const response = await fetch(`http://localhost:8001/api/user/cookie/${cookieValue}`);
-                // if (response.status === 200) {
-                //     login = response.text();
-                // }
-                user.login="test";
-                user.idUser="1";
+  return new Promise(async (resolve, reject) => {
+    let user = new UserVM();
+    if (document.cookie) {
+      console.log(document.cookie);
+      // get cookie
+      const sessionCookie = document.cookie
+        .split("; ")
+        .find((row) => row.startsWith("sessionCookie="));
+      const cookieValue = sessionCookie.split("=")[1];
 
-                console.log(user);
-            }
+      console.log(cookieValue);
+      if (cookieValue) {
+        const response = await fetch(`http://localhost:5241/api/user/cookie/${cookieValue}`, {
+          method: "POST",
+        });        
+        if (response.status === 200) {
+          user = response.json();
         }
-        resolve(user);
-    })
+        console.log(user);
+      }
+    }
+    resolve(user);
+  });
 }
